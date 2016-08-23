b6dccb234481ddf92d76de4f8bd44e5dc9d29f33
Fan Long <fanl@csail.mit.edu>
2016-08-22 23:15:13 -0400

    skip first line for python

--- cpright-append.py
+++ cpright-append.py
@@ -3,14 +3,22 @@ import os
 from sys import argv
 import getopt
 
-def append_header(fname, crlines, comment_token):
+def append_header(fname, crlines, comment_token, skip_first_line_comment):
     f = open(fname, "r");
+    first_line = f.readline();
     content = f.read();
     f.close();
     f = open(fname, "w");
+    skip_first_line = False;
+    if skip_first_line_comment and (first_line.find("//") == 0 or first_line.find("#") == 0 or first_line.find("/*") == 0):
+        skip_first_line = True;
+        f.write(first_line);
+        f.flush();
     for crline in crlines:
         print >> f, comment_token + crline.strip("\n");
     f.flush();
+    if not skip_first_line:
+        f.write(first_line);
     f.write(content);
     f.close();
 
@@ -35,6 +43,8 @@ for root, dirs, files in os.walk(tdir):
             continue;
         print "Append copyright header to " + root + "/" + fname;
         comment_token = "// ";
+        skip_first_line_comment = False;
         if ext == ".py":
             comment_token = "# ";
-        append_header(root + "/" + fname, crlines, comment_token);
+            skip_first_line_comment = True;
+        append_header(root + "/" + fname, crlines, comment_token, skip_first_line_comment);
